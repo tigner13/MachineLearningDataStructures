261f28e8f91b2717ae599ef85fd4779af6476aab
root <tigner@purdue.edu>
2016-09-22 16:17:36 -0400

    almost done

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,4 +1,22 @@
-In [848]: df - pd.read_csv('/mnt/c/Users/Owner/Desktop/Research/MachineLearningDataStructures')
+import pandas as pd
+import numpy as np
 
-In[848]: df
-Out[848]:
+df = pd.read_csv('house-votes-84.data')
+
+#deletes row if there is a NA
+def prob (party,i,c):
+    col = party[[i]]
+    col = col[col[[0]] != '?']
+    yes = col[col[[0]] == c]
+    return yes.count() / col.count()
+
+Republican = df[df['p'] == 'republican']
+probR = Republican.count() /  df.count()
+person = ['y','y','y','n','y','y','n','n','n','n','y','n','y','y','y','y']
+p = 1;
+bilnum = 1
+for vote in person:
+    p = p * prob(Republican, bilnum, vote)
+    bilnum +=1
+
+print p
