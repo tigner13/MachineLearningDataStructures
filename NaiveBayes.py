0ff0cf831bcb63ade7430e5bf561284e3d4a47de
root <tigner@purdue.edu>
2016-09-25 18:15:28 -0400

    I think I am done but going to test it agaist other code

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -5,24 +5,23 @@ df = pd.read_csv('house-votes-84.data')
 
 
 #deletes row if there is a NA
-def prob (party,i,c):
+def prob (party,i,vote):
     col = party[[i]]
     col = col[col[[0]] != '?']
-    yes = col[col[[0]] == 'y']
-    return (yes.count() / col.count())[0]
+    same = col[col[[0]] == vote]
+    return (same.count() / col.count())[0]
 
 def bayes(person, party):
     billnum =1;
     p=1
     Party = df[df['p'] == party]
     for vote in person:
-        p *= (prob(Party, billnum, vote))
+        (prob(Party, billnum, vote))
         billnum +=1
-    print p
-    p *= (Party.count() / df.count())[0]
-    print p
+    p = p* (Party.count() / df.count())[0]
+    return p
 
 
 
-person = ['y','y','y','n','y','y','n','n','n','n','y','n','y','y','y','y']
-bayes(person, 'democrat')
+person = ['n','y','y','y','y','y','n','n','n','y','y','n','y','y','n','n']
+print bayes(person, 'republican') / (bayes(person, 'democrat') + bayes(person, 'republican'))
