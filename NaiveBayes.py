f80ced0f1b9a0e85429ebaa4284a624e654dd0f6
root <tigner@purdue.edu>
2016-10-17 22:08:00 -0400

    added comments to each of the functions I wrote

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -15,13 +15,31 @@ class NaiveBayes(object):
         self.file = file
         self.df = pd.read_csv(file)
 
-    #deletes row if there is a NA
+    '''
+    Function Discription:
+    Inputs
+    party: a subset of the origional dataframe in which it only includes the members of the "party" party
+    i: the bill number you are anzalyzing
+    vote: if the person you are analyzing voted yes or no on the bill
+
+    Output: the probability that the person is in the party given their vote
+    '''
     def prob (self,party,i,vote):
         col = party[[i]]
         col = col[col[[0]] != '?']
         same = col[col[[0]] == vote]
         return (same.count() / col.count())[0]
 
+
+    '''
+    Function Discription:
+    Inputs
+    person: a list of all the votes of the peron your are analyzing
+    party: a string that repreents what party you are analyzing
+
+    Output
+    the probability that the person is in the party given all their votes
+    '''
     def bayes(self,person, party):
         billnum =1;
         p=1
@@ -33,14 +51,39 @@ class NaiveBayes(object):
         p = p * (Party.count() / df.count())[0]
         return p
 
-    def party(self,person):
+    '''
+    Function Discription:
+    Inputs
+    person: a list  of the votes of person you are analyzing
+
+    Output
+    The prediction if it is a republican or democrat
+    '''
+    def prediction(self,person):
         republican = self.bayes(person, 'republican')
         democrat = self.bayes(person, 'democrat')
-        if (republican > democrat): print "republican"
-        else: print "democrat"
+        if (republican > democrat): return 'republican'
+        else: return 'democrat'
+
+    '''
+    Inputs
+    person: a list of all atributes of the person you are analyzing
+
+    Output
+    1: if the program accuratly predict the person
+    0: if the program didn't accuratly predict the person
+    '''
+    def corect(self, person):
+        party = person[0]
+        person.pop(0)
+        if(party == self.prediction(person)): return 1
+        else: return 0
+
 
 n='n'
 y='y'
-person = [n,n,y,n,n,y,n,y,n,y,y,n,n,n,y,y]
+democrat = 'democrat'
+republican = 'republican'
+person = [democrat,n,n,y,n,n,y,n,y,n,y,y,n,n,n,y,y]
 program = NaiveBayes('house-votes-84.data')
-program.party(person)
+program.corect(person)
